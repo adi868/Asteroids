386dccc46d87c7ae7a11c097a59451d0fd8f93e2
Adina <alcianciotto@gmail.com>
2023-01-15 19:58:24 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,62 @@
+var ship;
+var rotation;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
 }
 
 function draw() {
-	background(0)
-	// put drawing code here
+  background(0);
+  ship.render();
+  ship.turn(0);
+  ship.update();
 }
 
-function Ship(){
-	
+function keyReleased() {
+  ship.setRotation(0);
+  ship.boosting = false;
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting = true;
+  }
+}
+
+function Ship() {
+  this.pos = createVector(width / 2, height / 2);
+  this.r = 20;
+  this.heading = 0;
+  this.rotation = 0;
+  this.vel = createVector(0, 0);
+  this.isBoosting = false;
+  this.boosting = function (b) {
+    this.isBoosting = b;
+  };
+  this.update = function () {
+    this.pos.add(this.vel);
+    this.vel.mult(0.95);
+  };
+  this.boost = function () {
+    var force = p5.Vector.fromAngle(this.heading);
+    this.vel.add(force);
+  };
+  this.render = function () {
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading + PI / 2);
+    noFill();
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  };
+  this.setRotation = function (a) {
+    this.rotation = a;
+  };
+  this.turn = function (angle) {
+    this.heading += this.rotation;
+  };
 }
